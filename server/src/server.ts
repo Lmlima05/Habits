c046c7be408f33032453edec0fcdd0844f21dc0d
Luan Melo <luanmelolima@gmail.com>
2024-03-31 19:58:39 -0300

    Configuração do `prisma` e config `cors` p/ front

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,24 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
 
 /**
   Metodo HTTP: Get, Post, Put, Patch, Delete
 */ 
 
-app.get('/hello', () => {
-  return 'Hello NLW'
+app.get('/hello', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
 })
 
 app.listen({
   port: 3333,
+}).then(() => {
+  console.log('HTTP Server running')
 })
