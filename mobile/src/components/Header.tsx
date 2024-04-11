ba86d11b6e1d7f4d16f19e6f88db98dc8c86d240
Luan Melo <luanmelolima@gmail.com>
2024-04-11 13:50:39 -0300

    Criando o HabitDay

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { View, TouchableOpacity, Text } from "react-native";
-import { Feather } from '@expo/vector-icons/Feather';
+import { AntDesign } from '@expo/vector-icons';
 import colors from 'tailwindcss/colors';
 
 import Logo from '../assets/logo.svg';
@@ -13,7 +13,7 @@ export function Header() {
         activeOpacity={0.7}
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
       > 
-        <Feather
+        <AntDesign
           name="plus"
           color={colors.violet[500]}
           size={20}
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,11 +1,27 @@
 import { View } from 'react-native';
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
 import { Header } from '../components/Header';
+import { HabitDay, DAY_SIZE } from '../components/HabitDay';
 
 export function Home(){
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
 
+      <View className="flex-row mt-6 mb-2">
+        {
+          weekDays.map((week, i) => (
+            <Text
+              key={`${weekDay}-${i}`}
+              className=
+            >
+            </Text>  
+          ))
+        }
+      </View>
+      <HabitDay />
     </View>
   )
 }
