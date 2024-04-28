c53f7eb76997d1f81163b473853ec96ed0f20cb1
Luan Melo <luanmelolima@gmail.com>
2024-04-28 08:16:05 -0300

    adicionando funcoes de delete no back

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -2,17 +2,16 @@ interface ProgressBarProps {
   progress: number
 }
 
-export function ProgressBar(props: ProgressBarProps) {
+export function ProgressBar({ progress }: ProgressBarProps) {
   return (
     <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
       <div
         role="progressbar"
+        aria-valuenow={progress}
+        style={{ width: `${progress}%` }}
         aria-label="Progresso de hábitos completados nesse dia"
-        aria-valuenow={props.progress}
-        className="h-3 rounded-xl bg-violet-600"
-        style={{ width: `${props.progress}%`
-        }}
+        className="h-3 rounded-xl bg-violet-600 transition-all"
       />
-   </div>
-  )
+    </div>
+  );
 }
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -5,74 +5,75 @@ import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-yea
 import { useEffect, useState } from 'react'
 
 const weekDays = [
-  'D',
-  'S',
-  'T',
-  'Q',
-  'Q',
-  'S',
-  'S',
-]
+  "D", 
+  "S", 
+  "T", 
+  "Q", 
+  "Q", 
+  "S", 
+  "S"
+];
 
-const sumaryDates = generateDatesFromYearBeginning()
-
-const minimumSummaryDatesSize = 18 * 7
-const amountOfDaysToFill = minimumSummaryDatesSize - sumaryDates.length
+const minimumSummaryDatesSize = 18 * 7;
+const summaryDates = generateDatesFromYearBeginning();
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
 type Summary = {
   id: string;
   date: string;
   amount: number;
   completed: number;
-}[]
+}[];
 
 export function SummaryTable() {
-  const [summary, setSummary] = useState<Summary>([])
+  const [summary, setSummary] = useState<Summary>([]);
 
   useEffect(() => {
-    api.get('summary').then(response => {
-      setSummary(response.data)
-    })
-  }, [])
+    api.get("summary").then((response) => setSummary(response.data));
+  }, []);
 
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
-        {weekDays.map((weekDay, i) => {
+        {weekDays.map((weekDay, index) => {
           return (
-            <div 
-              key={`${weekDay}-${i}`}
-               className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+            <div
+              key={`${weekDay}-${index}`}
+              className="text-zinc-400 text-xl font-bold h-10 w-10 flex items-center justify-center"
             >
               {weekDay}
             </div>
-          )
+          );
         })}
       </div>
 
-      <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {sumaryDates.map(date => {
-          const dayInSummary = summary.find(day => {
-            return dayjs(date).isSame(day.date, 'day')
-          })
+      <div className="grid grid-rows-6 grid-flow-col gap-3">
+        {summary.length > 0 &&
+          summaryDates.map((date) => {
+            const dayInSummary = summary.find((day) => {
+              return dayjs(date).isSame(day.date, "day");
+            });
 
-          return (
-            <HabitDay 
-              key={date.toString()} 
-              date={date}  
-              amount={dayInSummary?.amount} 
-              completed={dayInSummary?.completed} 
-            />)
-        })}
+            return (
+              <HabitDay
+                date={date}
+                key={date.toString()}
+                amount={dayInSummary?.amount}
+                defaultCompleted={dayInSummary?.completed}
+              />
+            );
+          })}
 
-        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill}).map((_, i) => {
-          return(
-            <div 
-            key={i}
-            className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
-          )
-        })}
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, index) => {
+            return (
+              <div
+                key={index}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            );
+          })}
       </div>
     </div>
-  )
+  );
 }
