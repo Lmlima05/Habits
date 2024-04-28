a394f912fdb72f6f1496f37fbfa6a017a2f6b063
Luan Melo <luanmelolima@gmail.com>
2024-04-28 07:09:20 -0300

    finalizando `HabitsLista`

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -4,48 +4,77 @@ import { useEffect, useState } from 'react'
 import { api } from '../lib/axios'
 import dayjs from 'dayjs'
 
-interface HabitsListProps {
-  date: Date
+interface HabitListProps {
+  date: Date;
+  onCompletedChange: (completed: number) => void;
 }
 
 interface HabitsInfo {
-  possibleHabits: {
-    id: number;
+  possibleHabits: Array<{
+    id: string;
     title: string;
     created_at: string;
-  }[],
-  completedHabits: string[]
+  }>;
+  completedHabits: string[];
 }
 
-export function HabitsList({ date }: HabitsListProps) {
-  const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>()
+export function HabitsList({ date, onCompletedChange }: HabitListProps) {
+  const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>();
+
+  const isDateInPast = dayjs(date).endOf("day").isBefore(new Date());
+
+  async function handleToggleHabit(habitId: string) {
+    const isHabitAlreadyCompleted =
+      habitsInfo!.completedHabits.includes(habitId);
+
+    await api.patch(`/habits/${habitId}/toggle`);
+
+    let completedHabits: string[] = [];
+
+    if (isHabitAlreadyCompleted) {
+      completedHabits = habitsInfo!.completedHabits.filter(
+        (id) => id !== habitId
+      );
+
+      setHabitsInfo({
+        completedHabits,
+        possibleHabits: habitsInfo!.possibleHabits,
+      });
+    } else {
+      completedHabits = [...habitsInfo!.completedHabits, habitId];
+    }
+
+    setHabitsInfo({
+      completedHabits,
+      possibleHabits: habitsInfo!.possibleHabits,
+    });
+
+    onCompletedChange(completedHabits.length);
+  }
 
   useEffect(() => {
-    api.get('day', {
-      params: {
-        date: date.toISOString(),
-      }
-    }).then(response => {
-      setHabitsInfo(response.data)
-    })
-  }, [])
-
-  const isDateInPast = dayjs(date)
-    .endOf('day')
-    .isBefore(new Date())
+    api
+      .get("day", {
+        params: {
+          date: date.toISOString(),
+        },
+      })
+      .then((response) => setHabitsInfo(response.data));
+  }, []);
 
   return (
     <div className="mt-6 flex flex-col gap-3">
-      {habitsInfo?.possibleHabits.map(habit => {
+      {habitsInfo?.possibleHabits.map((habit) => {
         return (
           <Checkbox.Root
             key={habit.id}
-            checked={habitsInfo.completedHabits.includes(habit.id)}
             disabled={isDateInPast}
-            className="flex items-center gap-3 group"
+            onCheckedChange={() => handleToggleHabit(habit.id)}
+            checked={habitsInfo.completedHabits.includes(habit.id)}
+            className="flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed"
           >
-            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-              <Checkbox.Indicator >
+            <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background">
+              <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
               </Checkbox.Indicator>
             </div>
@@ -53,9 +82,9 @@ export function HabitsList({ date }: HabitsListProps) {
             <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
               {habit.title}
             </span>
-          </Checkbox.Root>     
-        )
-      })}      
+          </Checkbox.Root>
+        );
+      })}
     </div>
-  )
+  );
 }
