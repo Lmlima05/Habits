41d974aabe9eddc3afb849aeb4c52c90bfef407e
Luan Melo <luanmelolima@gmail.com>
2024-04-28 07:22:18 -0300

    NewHabitForm

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -15,37 +15,35 @@ const avaiableWeekDays = [
 ]
 
 export function NewHabitForm() {
-  const [title, setTitle] = useState('')
+  const [title, setTitle] = useState("");
   const [weekDays, setWeekDays] = useState<number[]>([]);
 
   async function createNewHabit(event: FormEvent) {
-    event.preventDefault()
+    event.preventDefault();
 
     if (!title || weekDays.length === 0) {
-      return
+      return;
+    }
+
+    await api.post("habits", {
+      title,
+      weekDays,
+    });
+
+    setTitle("");
+    setWeekDays([]);
+
+    alert("Hábito criado com sucesso");
   }
-
-  await api.post('habits', {
-    title,
-    weekDays,
-  })
-
-  setTitle('')
-  setWeekDays([])
-
-  alert('Hábito criado com sucesso!')
-} 
 
   function handleToggleWeekDay(weekDay: number) {
     if (weekDays.includes(weekDay)) {
-      const weekDaysWithRemovedOne = weekDays.filter(day => day !== weekDay)
-
-      setWeekDays(weekDaysWithRemovedOne)
-     } else {
-      const weekDaysWithAddedOne = [...weekDays, weekDay]
-
-      setWeekDays(weekDaysWithAddedOne)
-     }
+      const weekDaysWithRemoveOne = weekDays.filter((day) => day !== weekDay);
+      setWeekDays(weekDaysWithRemoveOne);
+    } else {
+      const weekDaysWithAddedOne = [...weekDays, weekDay];
+      setWeekDays(weekDaysWithAddedOne);
+    }
   }
 
   return (
@@ -55,46 +53,47 @@ export function NewHabitForm() {
       </label>
 
       <input
-        type="text"
-        id="title"
-        placeholder="ex.: Exercícios, dormir bem, etc..."
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         autoFocus
+        id="title"
+        type="text"
         value={title}
-        onChange={event => setTitle(event.target.value)}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="ex.: Exercícios, dormir bem, etc..."
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
       />
 
-      <label htmlFor="" className="font-semibold leading-tight mt-4">
+      <label className="font-semibold leading-tight mt-4">
         Qual a recorrência?
       </label>
 
       <div className="flex flex-col gap-2 mt-3">
         {avaiableWeekDays.map((weekDay, index) => {
           return (
-            <Checkbox.Root 
-              key={weekDay} 
-              className="flex items-center gap-3 group"
+            <Checkbox.Root
+              key={weekDay}
               checked={weekDays.includes(index)}
               onCheckedChange={() => handleToggleWeekDay(index)}
+              className="flex items-center gap-3 group focus:outline-none"
             >
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-                <Checkbox.Indicator >
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background">
+                <Checkbox.Indicator>
                   <Check size={20} className="text-white" />
                 </Checkbox.Indicator>
               </div>
 
-            <span className=" text-white leading-tight">
-              {weekDay}
-            </span>
-         </Checkbox.Root>
-          )
-        })} 
+              <span className="text-white leading-tight">{weekDay}</span>
+            </Checkbox.Root>
+          );
+        })}
       </div>
 
-      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
+      <button
+        type="submit"
+        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-background"
+      >
         <Check size={20} weight="bold" />
         Confirmar
       </button>
     </form>
-  )
+  );
 }
